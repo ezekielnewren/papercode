4a96588dadbcf5a082e758444132d617d66cc93f
Ezekiel Newren <ezekielnewren@gmail.com>
2018-06-05 00:12:30 -0600

    can now make an executable jar. also excluded the ignore package in the jar creation stage

---
+++ src/main/java/com/ezekielnewren/papercode/Driver.java
@@ -0,0 +1,11 @@
+package com.ezekielnewren.papercode;
+
+public class Driver {
+
+	public static void main(String[] args) throws Exception {
+		PaperCode xxx = new PaperCode();
+		Thread.sleep(Long.MAX_VALUE);
+		xxx.close();
+	}
+	
+}
